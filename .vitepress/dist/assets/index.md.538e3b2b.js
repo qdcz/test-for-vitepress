8e3c33a8b73d163325cabf2e8e4cddef5d43fabf
qdcz <532911513@qq.com>
2022-02-14 00:08:49 +0800

    'create'

---
+++ .vitepress/dist/assets/index.md.538e3b2b.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as o}from"./app.737a200a.js";const u='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","lastUpdated":1644767545229}',s={},n=o("p",null,"666",-1),r=[n];function c(d,_,i,p,l,f){return a(),t("div",null,r)}var h=e(s,[["render",c]]);export{u as __pageData,h as default};
---
+++ docs/qdcz-note.md
@@ -0,0 +1,16 @@
+# 安装
+npm install vitepress --save
+
+# 在本地启动服务器
+npx vitepress
+
+# 构建静态文件 > .vitepress/dist
+npx vitepress build
+
+
+# 启动开发版
+npm run docs:dev  
+# 打包
+npm run build
+# 把打包结果在本地服务端运行
+npm run service
